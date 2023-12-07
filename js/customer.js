1224583486c2b0b319af760762c0ea4e6436af5d
Maloulou99 <113166624+Maloulou99@users.noreply.github.com>
2023-12-07 18:02:49 +0100

    Rettelser i index

--- js/customer.js
+++ js/customer.js
@@ -1,38 +1,25 @@
 async function createCustomer() {
-    try {
-      const customerForm = document.getElementById('customerForm');
-      const formData = new FormData(customerForm);
-  
-      const res = await fetch('/api/customer/create', {
-        method: 'POST',
-        body: formData,
-      });
-  
-      if (res.status >= 400) {
-        const error = new Error(`Error ${res.status} ${res.statusText}`);
-        throw error;
-      }
-  
-      const body = await res.json();
-      console.log('Kunde oprettet:', body);
-      skjulOpretKundeForm();
-    } catch (err) {
-      console.error(err.message);
+  try {
+    const customerForm = document.getElementById('customerForm');
+    const formData = new FormData(customerForm);
+
+    const res = await fetch('/api/customer/create', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (res.status >= 400) {
+      const error = await res.json();
+      throw new Error(`Error ${res.status}: ${error.message}`);
     }
+
+    const body = await res.json();
+    console.log('Customer created:', body);
+    skjulOpretKundeForm();
+  } catch (err) {
+    console.error('Error creating customer:', err.message);
   }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('addCustomerLink').addEventListener('click', function () {
-      console.log('Add customer link clicked');
-      visOpretKundeForm();
-    });
-  
-    document.getElementById('submitCustomerForm').addEventListener('click', function (event) {
-      event.preventDefault(); // Forhindrer formen i at blive indsendt på traditionel måde
-      createCustomer();
-    });
-  });
-  
+};
 
 // Funktion for at vise opret kunde formular
  function visOpretKundeForm() {
@@ -65,8 +52,13 @@ async function createCustomer() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('addCustomerLink').addEventListener('click', function () {
-    console.log('Add customer link clicked');
-    visOpretKundeForm();
-  });
+  var addCustomerLink = document.getElementById('addCustomerLink');
+  if (addCustomerLink) {
+      addCustomerLink.addEventListener('click', function () {
+          console.log('Add customer link clicked');
+          visOpretKundeForm();
+      });
+  }
+
+
 });
