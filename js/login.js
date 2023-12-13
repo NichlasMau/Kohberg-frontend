94a5fbb4d22f673299f24e8d7b03a498c8ecdc11
sami0880-kea <sami0880@stud.kea.dk>
2023-12-13 17:31:00 +0100

    Fixed minor login issues

--- js/login.js
+++ js/login.js
@@ -39,41 +39,5 @@ document.addEventListener('DOMContentLoaded', function() {
                     window.location.href = 'index.html'
                 }
             })
-
-        const email = document.getElementById('floatingInput').value;
-        const password = document.getElementById('floatingPassword').value;
-
-        const loginData = {
-            email: email,
-            password: password
-        };
-
-        fetch('https://kohberg-backend.azurewebsites.net/leader/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(loginData)
-        })
-        .then(response => {
-            if (response.ok) {
-                window.location.href = 'index.html';
-            } else {
-                const existingErrorMessages = document.querySelectorAll('.error-message');
-                existingErrorMessages.forEach(errorMessage => {
-                    errorMessage.remove();
-                });
-
-                const errorMessage = document.createElement('p');
-                errorMessage.textContent = 'Incorrect email or password. Please try again.';
-                errorMessage.classList.add('error-message');
-                errorMessage.style.color = 'red';
-                loginForm.appendChild(errorMessage);
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
     });
 });
